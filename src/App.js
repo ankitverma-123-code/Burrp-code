99da0fb00a6fc14a987e9d5c30b89fe16372106a
ankit verma <av4135850.19je0144@cse.iitism.ac.in>
2020-12-30 21:25:23 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import nature from './nature1.jpg';
 import './App.css';
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1 className ="head">WELCOME TO NATURE</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 className="bodyText">Nature, in the broadest sense, is the natural, physical, material world or universe. "Nature" can refer to the phenomena of the physical world, and also to life in general. The study of nature is a large, if not the only, part of science. Although humans are part of nature, human activity is often understood as a separate category from other natural phenomena.</h3>
+        
       </header>
+      <body>
+        
+      </body>
+      <footer className="footer">
+      <p className="para">
+          contact-us :)-
+        </p>
+        <p >Ⓒcopyright All Right's are reserved</p>
+        {/* <p >
+          All Right's are reserved
+        </p> */}
+        <p className="text">Made by ❤ Ankit</p>
+      </footer>
     </div>
   );
 }
